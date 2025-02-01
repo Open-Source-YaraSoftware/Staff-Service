e2c76779b657fa49d839f51ed2338239b54854da
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 18:40:43 -0500

    feat: add mechanic repository interface
    Add method to find all mechanics by WorkshopId

---
+++ src/main/java/com/workshopngine/platform/staffmanagement/staff/infrastructure/persistence/jpa/repositories/MechanicRepository.java
@@ -0,0 +1,13 @@
+package com.workshopngine.platform.staffmanagement.staff.infrastructure.persistence.jpa.repositories;
+
+import com.workshopngine.platform.staffmanagement.staff.domain.model.aggregates.Mechanic;
+import com.workshopngine.platform.staffmanagement.staff.domain.model.valueobjects.WorkshopId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface MechanicRepository extends JpaRepository<Mechanic, Long> {
+    Collection<Mechanic> findAllByWorkshopId(WorkshopId workshopId);
+}
