c7db788399a6fe4c4d79255df5ad888f0dccdd15
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 18:26:34 -0500

    feat: add method to convert string to enum  and fix type of fileId from Long to String

--- src/main/java/com/workshopngine/platform/staffmanagement/staff/domain/model/valueobjects/ESkillLevel.java
+++ src/main/java/com/workshopngine/platform/staffmanagement/staff/domain/model/valueobjects/ESkillLevel.java
@@ -3,5 +3,14 @@ package com.workshopngine.platform.staffmanagement.staff.domain.model.valueobjec
 public enum ESkillLevel {
     JUNIOR,
     SENIOR,
-    EXPERT
+    EXPERT;
+
+    public static ESkillLevel fromString(String level) {
+        return switch (level) {
+            case "JUNIOR" -> JUNIOR;
+            case "SENIOR" -> SENIOR;
+            case "EXPERT" -> EXPERT;
+            default -> throw new IllegalArgumentException("Invalid skill level: " + level);
+        };
+    }
 }
