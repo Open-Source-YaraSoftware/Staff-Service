a2cef02f01245cf5660cd8e78c600cf02d8c7b47
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 18:31:23 -0500

    feat(mechanic): add interfaces services for command and queries

---
+++ src/main/java/com/workshopngine/platform/staffmanagement/staff/domain/services/MechanicQueryService.java
@@ -0,0 +1,18 @@
+package com.workshopngine.platform.staffmanagement.staff.domain.services;
+
+import com.workshopngine.platform.staffmanagement.staff.domain.model.aggregates.Mechanic;
+import com.workshopngine.platform.staffmanagement.staff.domain.model.entities.SkillArea;
+import com.workshopngine.platform.staffmanagement.staff.domain.model.entities.SkillCertification;
+import com.workshopngine.platform.staffmanagement.staff.domain.model.entities.WorkDay;
+import com.workshopngine.platform.staffmanagement.staff.domain.model.queries.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface MechanicQueryService {
+    Optional<Mechanic> handle(GetMechanicByIdQuery query);
+    Collection<Mechanic> handle(GetAllMechanicsByWorkshopIdQuery query);
+    Collection<WorkDay> handle(GetAllWorkDaysByMechanicIdQuery query);
+    Collection<SkillArea> handle(GetAllSkillAreasByMechanicIdQuery query);
+    Collection<SkillCertification> handle(GetAllSkillCertificationsBySkillAreaIdAndMechanicIdQuery query);
+}
