3cdfeda28de99181153e68c792d4a3bfd06f6d43
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 18:35:26 -0500

    chore: enable feign client in Spring Boot Application

--- src/main/java/com/workshopngine/platform/staffmanagement/StaffManagementApplication.java
+++ src/main/java/com/workshopngine/platform/staffmanagement/StaffManagementApplication.java
@@ -2,10 +2,12 @@ package com.workshopngine.platform.staffmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class StaffManagementApplication {
 
 	public static void main(String[] args) {
