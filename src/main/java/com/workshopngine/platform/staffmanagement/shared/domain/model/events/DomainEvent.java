bafa8b94d1264f9f377900038f4390998a40a138
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 05:41:19 -0500

    feat: add classes for auditable aggregate and entity, add domain event base and message resource

---
+++ src/main/java/com/workshopngine/platform/staffmanagement/shared/domain/model/events/DomainEvent.java
@@ -0,0 +1,6 @@
+package com.workshopngine.platform.staffmanagement.shared.domain.model.events;
+
+import java.io.Serializable;
+
+public interface DomainEvent extends Serializable {
+}
---
+++ src/main/java/com/workshopngine/platform/staffmanagement/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.workshopngine.platform.staffmanagement.shared.interfaces.rest.resources;
+
+public record MessageResource(String message) {
+}
