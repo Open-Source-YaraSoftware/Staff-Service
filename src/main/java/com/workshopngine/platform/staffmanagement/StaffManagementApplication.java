c3f5c3b2d6a1f2b8ec50c7f7ef3cd20fc3dda17a
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 06:19:11 -0500

    chore: enable jpa auditing

--- src/main/java/com/workshopngine/platform/staffmanagement/StaffManagementApplication.java
+++ src/main/java/com/workshopngine/platform/staffmanagement/StaffManagementApplication.java
@@ -2,8 +2,10 @@ package com.workshopngine.platform.staffmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StaffManagementApplication {
 
 	public static void main(String[] args) {
