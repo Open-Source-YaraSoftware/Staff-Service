95b9843b8f92b972db6212f2a698fb57bc5bb3bb
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-03-05 18:22:50 -0500

    feat(dto): add builder annotation to CreateWorkDayResource and CreateSkillAreaResource
    Enabled builder pattern for CreateWorkDayResource and CreateSkillAreaResource using Lombok's @Builder annotation.

--- src/main/java/com/workshopngine/platform/staffmanagement/staff/interfaces/rest/dto/CreateSkillAreaResource.java
+++ src/main/java/com/workshopngine/platform/staffmanagement/staff/interfaces/rest/dto/CreateSkillAreaResource.java
@@ -1,5 +1,8 @@
 package com.workshopngine.platform.staffmanagement.staff.interfaces.rest.dto;
 
+import lombok.Builder;
+
+@Builder
 public record CreateSkillAreaResource(
         String area,
         String level
--- src/main/java/com/workshopngine/platform/staffmanagement/staff/interfaces/rest/dto/CreateWorkDayResource.java
+++ src/main/java/com/workshopngine/platform/staffmanagement/staff/interfaces/rest/dto/CreateWorkDayResource.java
@@ -1,7 +1,10 @@
 package com.workshopngine.platform.staffmanagement.staff.interfaces.rest.dto;
 
+import lombok.Builder;
+
 import java.time.LocalTime;
 
+@Builder
 public record CreateWorkDayResource(
         String day,
         LocalTime startAt,
