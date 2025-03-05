8db9029fa118be838962448c8660bc1277a3a996
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-03-05 17:13:10 -0500

    feat(test): integrate Testcontainers for MySQL
    Added Testcontainers dependencies and configured MySQL test container in StaffManagementApplicationTests to enable isolated database testing.

--- src/test/java/com/workshopngine/platform/staffmanagement/StaffManagementApplicationTests.java
+++ src/test/java/com/workshopngine/platform/staffmanagement/StaffManagementApplicationTests.java
@@ -1,10 +1,20 @@
 package com.workshopngine.platform.staffmanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class StaffManagementApplicationTests {
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0");
 
 	@Test
 	void contextLoads() {
